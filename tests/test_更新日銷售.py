5945a726b9c9aec749c522b2a6299af756633d7f
tsung-han <d5269357812@gmail.com>
2022-07-18 13:22:44 +0800

    test:test_更新日銷售() 100 get日銷售 ftp_每日銷售報表

--- tests/test_更新日銷售.py
+++ tests/test_更新日銷售.py
@@ -1,14 +1,18 @@
+'''更新日銷售 spec
+作者:朧月
+github:https://github.com/LonYui
+'''
+import os
+from datetime import datetime
+
+
 def test_更新日銷售():
     '''跑 7/3 更新，確定更加好有更新'''
 
-    # 建立測試資料
-    import os
-    os.system('cp test_更新日銷售_測試資料/更加好_銷售報表07.02.xlsx  db_test/更加好_銷售報表07.xlsx')
+    os.system('cp test_更新日銷售_測試資料/更加好_銷售報表07.02.xlsx  db_test/更加好_銷售報表07.xlsx') # 建立測試資料
     os.system('cp test_更新日銷售_測試資料/設定.json  db_test')
 
-    # 執行主程式()
-    """驗證填寫前後"""
-    from openpyxl import load_workbook
+    from openpyxl import load_workbook    # 執行主程式()
 
     更加好報表_機器人_wb = load_workbook('tests/db_test/更加好_銷售報表07.xlsx')
     更加好報表_機器人 = 更加好報表_機器人_wb.active
@@ -16,22 +20,41 @@ def test_更新日銷售():
     更加好報表_手打_wb = load_workbook('tests/7月/更加好_銷售報表07.03.xlsx')
     更加好報表_手打 = 更加好報表_手打_wb.active
 
-    """日銷售正確"""
+
     更加好報表_手打_日銷售row = []
     for row in 更加好報表_手打.iter_rows(min_row=11, max_row=11, min_col=3,
-                               max_col=3 + 41, values_only=True):
+                                  max_col=3 + 41, values_only=True):
         更加好報表_手打_日銷售row.append(row)
     更加好報表_機器人_日銷售row = []
     for row in 更加好報表_機器人.iter_rows(min_row=11, max_row=11, min_col=3,
-                               max_col=3 + 41, values_only=True):
+                                   max_col=3 + 41, values_only=True):
         更加好報表_機器人_日銷售row.append(row)
-    assert 更加好報表_機器人_日銷售row==更加好報表_手打_日銷售row
+    assert 更加好報表_機器人_日銷售row == 更加好報表_手打_日銷售row    # 日銷售正確
 
-
-
-    # 刪除測試資料
-    os.system('rm db_test/更加好_銷售報表07.xlsx ')
+    os.system('rm db_test/更加好_銷售報表07.xlsx ')     # 刪除測試資料
     os.system('rm db_test/設定.json ')
 
 def test_ftp_每日銷售報表():
-   assert True
+    '''下載本日檔案到db_test format:"219720220703sal.csv"'''
+    from 更新日銷售 import ftp_每日銷售報表
+
+    ftp_每日銷售報表()
+    open(
+        f'db_test/2197{datetime.date.today().strftime("%Y%m%d")}sal.csv')
+
+
+
+def test_get_日銷售():
+    '''get 更加好 7/3 日銷售 from sal'''
+    input_outputs=[('3030044',142),('33030024',72)]  # 0.更加好 ,1.穎軒　台塑蔬菜秘漬小黃瓜 / ５００ｇ
+
+    os.system('cp test_更新日銷售_測試資料/219720220703sal.csv db_test/下載/219720220703sal.csv') # 新增測試資料到ｄb
+
+    from 更新日銷售 import get_日銷售
+
+    for input_output in input_outputs:
+        input , output = input_output
+        assert get_日銷售(input) == output
+
+    os.system('rm db_test/下載/219720220703sal.csv') # 刪除測試資料
+    assert False
