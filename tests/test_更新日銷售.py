840041da9f69f699d16f77f4b67e951df516f548
tsung-han <d5269357812@gmail.com>
2022-07-20 21:12:01 +0800

    feat: job2 get_日銷售 (non)

--- tests/test_更新日銷售.py
+++ tests/test_更新日銷售.py
@@ -47,20 +47,25 @@ def test_ftp_每日銷售報表():
         open(f'db_test/下載/{廠商編號}{datetime.date.today().strftime("%Y%m%d")}sal.csv')
 
     os.system('rm db_test/設定.json ')
+    os.system('rm -r db_test/下載/')
+    os.system('mkdir db_test/下載')
 
 
 
 def test_get_日銷售():
     '''get 更加好 7/3 日銷售 from sal'''
-    input_outputs=[('3030044',142),('33030024',72)]  # 0.更加好 ,1.穎軒　台塑蔬菜秘漬小黃瓜 / ５００ｇ
+    input_outputs=[({'貨號':'03030044','日期':'2022-07-03'},142),({'貨號':'33030024','日期':'2022-07-03'},72)]  # 0.更加好 ,1.穎軒　台塑蔬菜秘漬小黃瓜 / ５００ｇ
 
     os.system('cp test_更新日銷售_測試資料/219720220703sal.csv db_test/下載/219720220703sal.csv') # 新增測試資料到ｄb
+    os.system('cp test_更新日銷售_測試資料/設定.json  db_test')
+
 
     from 更新日銷售 import get_日銷售
 
     for input_output in input_outputs:
         input , output = input_output
-        assert get_日銷售(input) == output
+        assert get_日銷售(**input) == output
 
     os.system('rm db_test/下載/219720220703sal.csv') # 刪除測試資料
-    assert False
+    os.system('rm db_test/設定.json ')
+
--- 更新日銷售.py
+++ 更新日銷售.py
@@ -1,23 +1,41 @@
 ''''''
 import datetime
+import json
 import os
 
-db_path=os.getenv('db_path')
+db_path=os.getenv('db_path') if os.getenv('db_path') else 'db' #default = db
+
+設定_f = open(f'{db_path}/設定.json')
+設定 = json.load(設定_f)
+
 def ftp_每日銷售報表():
     ''
-    import os
-    db_path = os.getenv('db_path')
-    設定_f = open(f'{db_path}/設定.json')
-
-    import json
-    設定 = json.load(設定_f)
     from ftplib import FTP
     ftp = FTP('pngscftpsec.tradevan.com.tw')
-    for ptb in 設定:
-        file_name = f"{ptb['ptvcod']}{datetime.date.today().strftime('%Y%m%d')}sal.csv"
-        ftp.login(ptb['ptvcod'], ptb['ftp密碼'])
+    for ptv in 設定:
+        file_name = f"{ptv['ptvcod']}{datetime.date.today().strftime('%Y%m%d')}sal.csv"
+        ftp.login(ptv['ptvcod'], ptv['ftp密碼'])
         ftp.retrbinary(f"RETR {file_name}",open(file_name, 'wb').write)
         os.system(f'mv {file_name} {db_path}/下載/{file_name}')
+    ftp.close()
 
-def get_日銷售(貨號):
+def get_日銷售(貨號,日期=datetime.date.today().isoformat()):
     ''''''
+    日期_datetime = datetime.datetime.fromisoformat(日期)
+    # 銷售報表_f = open(    f"{db_path}/下載/{_get_ptv_by貨號(貨號)}{日期_datetime.strftime('%Y%m%d')}sal.csv",'r', encoding='Big5')
+    import pandas
+
+    數據報表 = pandas.read_csv(
+        f"{db_path}/下載/{_get_ptv_by貨號(貨號)}{日期_datetime.strftime('%Y%m%d')}sal.csv",
+        encoding='Big5', dtype={'PRDTCODE': 'str'})
+
+    return 數據報表[['PRDTMLQY', 'PRDTCODE']][數據報表['PRDTCODE'] == 貨號]['PRDTMLQY'].sum()
+
+
+def _get_ptv_by貨號(貨號):
+    for ptv in 設定:
+        for 廠商 in ptv['廠商']:
+            if 貨號 in 廠商['商品清單']:
+                return ptv['ptvcod']
+
+    return None
