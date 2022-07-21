6b5656f71c2a4cd17911c3471233fd161bbac368
tsung-han <d5269357812@gmail.com>
2022-07-21 21:40:50 +0800

    fix:更新日銷售 日期要剪一天唷

--- tests/test_更新日銷售.py
+++ tests/test_更新日銷售.py
@@ -7,17 +7,17 @@ import datetime
 
 
 def test_更新日銷售():
-    '''跑 7/3 更新，確定更加好有更新'''
+    '''跑 7/4 更新，確定更加好有更新'''
 
     os.system('cp test_更新日銷售_測試資料/匯恩_銷售報表07.02.xlsx  db_test/匯恩_銷售報表07.xlsx') # 建立測試資料
     os.system('cp test_更新日銷售_測試資料/更加好_銷售報表07.02.xlsx  db_test/更加好_銷售報表07.xlsx')
     os.system('cp test_更新日銷售_測試資料/設定.json  db_test')
 
     from 更新日銷售 import main
-    main('2022-07-03')
-    _比對_byrow('更加好',11)
-    _比對_byrow('匯恩',11)
-    _比對_byrow('匯恩',11+3)
+    main('2022-07-04')
+    _比對_byrow('更加好', 11)
+    _比對_byrow('匯恩', 11)
+    _比對_byrow('匯恩', 11 + 3)
 
 
     # 更加好報表_機器人_wb = load_workbook('tests/db_test/更加好_銷售報表07.xlsx')
@@ -40,6 +40,9 @@ def test_更新日銷售():
     os.system('rm db_test/匯恩_銷售報表07.xlsx ')     # 刪除測試資料
     os.system('rm db_test/更加好_銷售報表07.xlsx ')     # 刪除測試資料
     os.system('rm db_test/設定.json ')
+    os.system('rm -r db_test/下載/')
+    os.system('mkdir db_test/下載')
+
 
 def test_ftp_每日銷售報表():
     '''下載本日檔案到db_test format:"219720220703sal.csv"'''
@@ -67,8 +70,8 @@ def test_ftp_每日銷售報表():
 
 
 def test_get_日銷售():
-    '''get 更加好 7/3 日銷售 from sal'''
-    input_outputs=[({'貨號':'03030044','日期':'2022-07-03'},142)]  # 0.更加好 ,
+    '''get 更加好 7/2 日銷售 from sal'''
+    input_outputs=[({'貨號':'03030044','日期':'2022-07-02'},142)]  # 0.更加好 ,
 
     os.system('cp test_更新日銷售_測試資料/219720220703sal.csv db_test/下載/219720220703sal.csv') # 新增測試資料到ｄb
     os.system('cp test_更新日銷售_測試資料/設定.json  db_test')
@@ -83,21 +86,21 @@ def test_get_日銷售():
     os.system('rm db_test/下載/219720220703sal.csv') # 刪除測試資料
     os.system('rm db_test/設定.json ')
 
-def _比對_byrow(廠商, row):
+def _比對_byrow(廠商, row_num):
     '''todo 可以比對多個商品 且只讀取一個 config'''
     from openpyxl import load_workbook  # 執行主程式()
-    報表_機器人_wb = load_workbook(f'tests/db_test/{廠商}_銷售報表07.xlsx')
+    報表_機器人_wb = load_workbook(f'db_test/{廠商}_銷售報表07.xlsx')
     報表_機器人 = 報表_機器人_wb.active
-    報表_手打_wb = load_workbook(f'tests/7月/{廠商}_銷售報表07.03.xlsx')
+    報表_手打_wb = load_workbook(f'7月/{廠商}_銷售報表07.03.xlsx')
     報表_手打 = 報表_手打_wb.active
     報表_手打_日銷售row = []
-    for row in 報表_手打.iter_rows(min_row=row, max_row=row, min_col=3,
-                               max_col=3 + 41, values_only=True):
+    for row in 報表_手打.iter_rows(min_row=row_num, max_row=row_num, min_col=3,
+                                   max_col=3 + 41, values_only=True):
         報表_手打_日銷售row.append(row)
     報表_機器人_日銷售row = []
-    for row in 報表_機器人.iter_rows(min_row=row, max_row=row,
-                                min_col=3,
-                                max_col=3 + 41,
-                                values_only=True):
+    for row in 報表_機器人.iter_rows(min_row=row_num, max_row=row_num,
+                                    min_col=3,
+                                    max_col=3 + 41,
+                                    values_only=True):
         報表_機器人_日銷售row.append(row)
     assert 報表_機器人_日銷售row == 報表_手打_日銷售row  # 日銷售正確
--- 更新日銷售.py
+++ 更新日銷售.py
@@ -1,4 +1,4 @@
-''''''
+'''Warn:七月3號報表拿的是七月2號的資料'''
 import datetime
 import json
 import math
@@ -15,22 +15,23 @@ def main(日期=datetime.date.today().isoformat()):
     for ptv in 設定:
         for 廠商 in ptv['廠商']:
             import openpyxl
-            銷售報表_f = open(f'{db_path}/{廠商["名稱"]}銷售報表07.xlsx')
-            銷售報表 = openpyxl.load_workbook(銷售報表_f)
+            銷售報表_wb = openpyxl.load_workbook(f'{db_path}/{廠商["名稱"]}_銷售報表07.xlsx')
+            銷售報表 = 銷售報表_wb.active
             # for index in range(len(廠商['商品清單'])):
             for index,商品code in enumerate(廠商['商品清單']):
                 日期_datetime = datetime.datetime.fromisoformat(日期)
-                禮拜幾 = 日期_datetime.weekday() #0 1 2 3 4 5 6
-                銷售報表[f'{chr(ord("C")+7*_第幾週(日期)+禮拜幾)}{11+4*index}']=get_日銷售(商品code)
-            銷售報表.save()
+                日期減一_datetime = 日期_datetime - datetime.timedelta(1)
+                日期減一禮拜幾 = 日期減一_datetime.weekday() #0 1 2 3 4 5 6
+                日期減一 = 日期減一_datetime.date().isoformat()
+                銷售報表[f'{chr(ord("C")+7*_第幾週(日期減一_datetime)+日期減一禮拜幾)}{11+4*index}']=get_日銷售(商品code,日期=日期減一)
+            銷售報表_wb.save(f'{db_path}/{廠商["名稱"]}_銷售報表07.xlsx')
     None
 
 
-def _第幾週(日期):
+def _第幾週(日期_datetime):
     ''' output: 0 1 2 3 4'''
-    日期_datetime = datetime.datetime.fromisoformat(日期)
     本月一號禮拜幾 = datetime.date(日期_datetime.year,日期_datetime.month,1).weekday()
-    return math.floor((日期_datetime.day + 本月一號禮拜幾)/7)
+    return math.floor((日期_datetime.day +7- 本月一號禮拜幾)/7)
 
 
 if __name__ == "__main__":
@@ -53,11 +54,11 @@ def ftp_每日銷售報表(日期=datetime.date.today().isoformat()):
 def get_日銷售(貨號,日期=datetime.date.today().isoformat()):
     ''''''
     日期_datetime = datetime.datetime.fromisoformat(日期)
-    # 銷售報表_f = open(    f"{db_path}/下載/{_get_ptv_by貨號(貨號)}{日期_datetime.strftime('%Y%m%d')}sal.csv",'r', encoding='Big5')
+    日期加一_datetime = 日期_datetime + datetime.timedelta(1)
     import pandas
 
     數據報表 = pandas.read_csv(
-        f"{db_path}/下載/{_get_ptv_by貨號(貨號)}{日期_datetime.strftime('%Y%m%d')}sal.csv",
+        f"{db_path}/下載/{_get_ptv_by貨號(貨號)}{日期加一_datetime.strftime('%Y%m%d')}sal.csv",
         encoding='Big5', dtype={'PRDTCODE': 'str'})
 
     return 數據報表[['PRDTMLQY', 'PRDTCODE']][數據報表['PRDTCODE'] == 貨號]['PRDTMLQY'].sum()
